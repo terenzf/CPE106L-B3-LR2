bcb49e1e879f3c93663b319c9374ee886acbcab3
Terenz Flores <100450790+terenzf@users.noreply.github.com>
2022-03-13 19:24:32 +0800

    Add files via upload

---
+++ LR2_2.py
@@ -0,0 +1,23 @@
+enterfile = input("Enter the file name: ")
+file = open(enterfile, 'r')
+lineCounter = 0
+
+for line in file:
+    lineCounter = lineCounter + 1
+
+print("There are", lineCounter, "lines in this file")
+
+while True:
+    lineNumber = 0
+
+    num = int(input("Enter a line number (0 to quit): "))
+    if num >=1 and num <= lineCounter:
+        file = open(enterfile, 'r')
+        for lines in file:
+            lineNumber = lineNumber + 1
+            if lineNumber == num:
+                print(lines)
+    else:
+        if num == 0:
+            print("The program has been terminated. Thank you.")
+            break
