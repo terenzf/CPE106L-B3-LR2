7ed5060f025830d0597d16278a04fe200b39b764
Terenz Flores <100450790+terenzf@users.noreply.github.com>
2022-03-13 19:49:08 +0800

    Update LR2_2.py

--- LR2_2.py
+++ LR2_2.py
@@ -1,23 +1,15 @@
 fileInput = input("Enter a file name: ")
-file = open(fileInput, 'r')
-lineCounter = 0
 
-for line in file:
-    lineCounter = lineCounter + 1
+with open(fileInput) as f:
+    lines = [line.rstrip() for line in f]
 
-print("There are", lineCounter, "lines in this file")
+print("There are", len(lines),"lines in this text file.")
 
 while True:
-    lineNumber = 0
+    num = int(input("Enter a line number [0 = quit]: "))
 
-    num = int(input("Enter a line number (0 to quit): "))
-    if num >=1 and num <= lineCounter:
-        file = open(fileInput, 'r')
-        for lines in file:
-            lineNumber = lineNumber + 1
-            if lineNumber == num:
-                print(lines)
-    else:
-        if num == 0:
-            print("The program has been terminated. Thank you.")
-            break
+    if num > 0 and num < len(lines) + 1:
+        print(lines[num - 1]) 
+    elif num == 0:
+        print('The program has been terminated. Thank you.')
+        break
